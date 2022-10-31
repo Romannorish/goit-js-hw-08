936c9150c76e6e19c10d83a07b3c0cfb5eb4c991
Roman Denysenko <97247092+Romannorish@users.noreply.github.com>
2022-11-01 00:03:54 +0300

    commit 1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,44 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
+
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryImg = createGalleryImg(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryImg);
+
+console.log(createGalleryImg(galleryItems));
+
+function createGalleryImg(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+        <div class="gallery__item">
+           <a class="gallery__link" href="${original}">
+           <img
+           class="gallery__image"
+           src="${preview}"
+           data-source="${original}"
+           alt="${description}"
+           />
+           </a>
+        </div>
+      `;
+    }).join("");
+};
+
+
+new SimpleLightbox ('.gallery a', { 
+  nav: true,
+  caption: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+SimpleLightbox();
+
